b3db11e2ae38a9952cf4be5b688a20812a30e9a3
AliF50 <alihassan@post.com>
2016-12-31 18:18:26 -0500

    'work'

---
+++ src/routes.js
@@ -0,0 +1,46 @@
+(function () {
+    'use strict';
+
+    angular.module('MenuApp')
+        .config(RoutesConfig);
+
+    RoutesConfig.$inject = ['$stateProvider', '$urlRouterProvider'];
+
+    function RoutesConfig($stateProvider, $urlRouterProvider) {
+        $urlRouterProvider.otherwise('/'); //Redirect to home page if no other URL matches
+        //Set up UI states
+        $stateProvider
+
+            .state('home', {
+            url: '/',
+            templateUrl: 'src/menuapp/templates/home.template.html'
+        })
+
+        //categories state
+        .state('categories', {
+            url: '/categories',
+            templateUrl: 'src/menuapp/templates/categories.template.html',
+            controller: 'CategoriesController as categoriesCtrl',
+            resolve: {
+                categories: ['MenuDataService', function (MenuDataService) {
+                    return MenuDataService.getAllCategories().then(function (response) {
+                        return response.data;
+                    });
+                }]
+            }
+        })
+
+        .state('items', {
+            url: '/items/{category}',
+            templateUrl: 'src/menuapp/templates/items.template.html',
+            controller: 'ItemsController as itemsCtrl',
+            resolve: {
+                items: ['$stateParams', 'MenuDataService', function ($stateParams, MenuDataService) {
+                    return MenuDataService.getItemsForCategory($stateParams.category).then(function (response) {
+                        return response.data.menu_items;
+                    });
+                }]
+            }
+        });
+    }
+})();
